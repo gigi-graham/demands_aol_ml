1f07e20d24c3fcf517adf86e481913ec78dc9938
gigi-graham <indira.revatta@gmail.com>
2025-05-31 22:38:14 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template
+import joblib
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+model = joblib.load('model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        inventory = int(request.form["inventory"])
+        sold = int(request.form["sold"])
+        ordered = int(request.form["ordered"])
+        price = float(request.form["price"])
+        discount = int(request.form["discount"])
+        holiday = int(request.form["holiday"])
+        competitor = float(request.form["competitor"])
+
+        categories = ["Clothing", "Electronics", "Furniture", "Groceries", "Toys"]
+        category_input = request.form["category"]
+        category = [1.0 if category_input == c else 0.0 for c in categories]
+
+        regions = ["East", "North", "South", "West"]
+        region_input = request.form["region"]
+        region = [1.0 if region_input == r else 0.0 for r in regions]
+
+        weathers = ["Cloudy", "Rainy", "Snowy", "Sunny"]
+        weather_input = request.form["weather"]
+        weather = [1.0 if weather_input == w else 0.0 for w in weathers]
+
+        seasons = ["Autumn", "Spring", "Summer", "Winter"]
+        season_input = request.form["season"]
+        season = [1.0 if season_input == s else 0.0 for s in seasons]
+
+        features = [inventory, sold, ordered, price, discount, holiday, competitor]
+        features += category + region + weather + season
+
+        category_feature_names = ["Category_Clothing", "Category_Electronics", "Category_Furniture", "Category_Groceries", "Category_Toys"]
+        region_feature_names = ["Region_East", "Region_North", "Region_South", "Region_West"]
+        weather_feature_names = ["Weather Condition_Cloudy", "Weather Condition_Rainy", "Weather Condition_Snowy", "Weather Condition_Sunny"]
+        season_feature_names = ["Seasonality_Autumn", "Seasonality_Spring", "Seasonality_Summer", "Seasonality_Winter"]
+
+        columns = ["Inventory Level", "Units Sold", "Units Ordered", "Price", "Discount", "Holiday/Promotion", "Competitor Pricing"] + category_feature_names + region_feature_names + weather_feature_names + season_feature_names
+
+        df = pd.DataFrame([features], columns=columns)
+
+        prediction = model.predict(df)[0]
+
+        return render_template("predict.html", prediction_text=f"Predicted result: {round(prediction, 2)} units!")
+
+    return render_template("predict.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
