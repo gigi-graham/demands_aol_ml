5377a4c463f8d84504df142e5b380f3681ba5ab7
gigi-graham <indira.revatta@gmail.com>
2025-06-06 22:43:01 +0700

    fixed model and scaler

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# hai
-
 model = joblib.load('model.pkl')
+scaler = joblib.load('scaler.pkl')
 
 @app.route('/')
 def home():
@@ -32,25 +31,30 @@ def predict():
         region_input = request.form["region"]
         region = [1.0 if region_input == r else 0.0 for r in regions]
 
-        weathers = ["Cloudy", "Rainy", "Snowy", "Sunny"]
-        weather_input = request.form["weather"]
-        weather = [1.0 if weather_input == w else 0.0 for w in weathers]
-
         seasons = ["Autumn", "Spring", "Summer", "Winter"]
         season_input = request.form["season"]
         season = [1.0 if season_input == s else 0.0 for s in seasons]
 
+        weathers = ["Cloudy", "Rainy", "Snowy", "Sunny"]
+        weather_input = request.form["weather"]
+        weather = [1.0 if weather_input == w else 0.0 for w in weathers]
+
         features = [inventory, sold, ordered, price, discount, holiday, competitor]
-        features += category + region + weather + season
+        features += category + region + season + weather
 
-        category_feature_names = ["Category_Clothing", "Category_Electronics", "Category_Furniture", "Category_Groceries", "Category_Toys"]
-        region_feature_names = ["Region_East", "Region_North", "Region_South", "Region_West"]
-        weather_feature_names = ["Weather Condition_Cloudy", "Weather Condition_Rainy", "Weather Condition_Snowy", "Weather Condition_Sunny"]
-        season_feature_names = ["Seasonality_Autumn", "Seasonality_Spring", "Seasonality_Summer", "Seasonality_Winter"]
+        # category_feature_names = ["Category_Clothing", "Category_Electronics", "Category_Furniture", "Category_Groceries", "Category_Toys"]
+        # region_feature_names = ["Region_East", "Region_North", "Region_South", "Region_West"]
+        # weather_feature_names = ["Weather Condition_Cloudy", "Weather Condition_Rainy", "Weather Condition_Snowy", "Weather Condition_Sunny"]
+        # season_feature_names = ["Seasonality_Autumn", "Seasonality_Spring", "Seasonality_Summer", "Seasonality_Winter"]
 
-        columns = ["Inventory Level", "Units Sold", "Units Ordered", "Price", "Discount", "Holiday/Promotion", "Competitor Pricing"] + category_feature_names + region_feature_names + weather_feature_names + season_feature_names
+        # columns = ["Inventory Level", "Units Sold", "Units Ordered", "Price", "Discount", "Holiday/Promotion", "Competitor Pricing"] + category_feature_names + region_feature_names + weather_feature_names + season_feature_names
+
+        columns = joblib.load("columns.pkl")
 
         df = pd.DataFrame([features], columns=columns)
+
+        numeric_cols = ['Inventory Level', 'Units Sold', 'Units Ordered', 'Price', 'Discount', 'Competitor Pricing']
+        df[numeric_cols] = scaler.transform(df[numeric_cols])
 
         prediction = model.predict(df)[0]
 
